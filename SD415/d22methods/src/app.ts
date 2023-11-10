86f7831f2125fac516bf758af6cd2ecc3b3672aa
Mekdes Negi <mekdes.negi@miu.edu>
2023-11-10 08:40:51 -0600

    change on string

--- SD415/d22methods/src/app.ts
+++ SD415/d22methods/src/app.ts
@@ -1,2 +1,11 @@
 //write any code you want to test here and run with npm run app
 
+//  const arr:number[]=[1,5,16,3, 108];
+//  arr.forEach((num)=>{
+//     if(num%2==0){
+//         console.log(num);
+//     }
+//  });
+//const arr=[1,2,3,4,5];
+const numbers = [1, 5, 18, 2, 77, 108];
+numbers.filter(number=>number%2==0);
--- SD415/d23datatypes/src/app.ts
+++ SD415/d23datatypes/src/app.ts
@@ -3,35 +3,39 @@
  * 
  * @param {*} str 
  */
-export function ucFirst(str:string) {
- return str.charAt(0).toUpperCase()+str.slice(1);
+export function ucFirst(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 
 
+}
+
+/**
+ * 
+ * @param {*} str 
+ */
+export function checkSpam(str: string, keywords: string[]): boolean {
+  const Str1 = str.toLowerCase();
+
+  for (const keyword of keywords) {
+    if (Str1.includes(keyword)) {
+      return true;
+    }
   }
 
-  /**
-   * 
-   * @param {*} str 
-   */
-  export  function checkSpam(str:string) {
-    const lowerCaseStr = str.toLowerCase();
-    return lowerCaseStr.includes('viagra') || lowerCaseStr.includes('xxx');
-
-
-
-  }
+  return false;
+}
 
 /**
  * 
  * @param {*} str 
  * @param {*} maxlength 
  */
-export   function truncate(str:string, maxlength:number) {
-        if(str.length>maxlength){
-  return  str.slice(0, maxlength - 1) + '…';
-} else {
-  return str;
-}
+export function truncate(str: string, maxlength: number) {
+  if (str.length > maxlength) {
+    return str.slice(0, maxlength - 1) + '…';
+  } else {
+    return str;
+  }
 }
 
 
@@ -42,15 +46,15 @@ export   function truncate(str:string, maxlength:number) {
  * @returns {number} the total of the maximal subarray
  
  */
-export function getMaxSubSum(arr:number[]) {
+export function getMaxSubSum(arr: number[]) {
 
   let maxSum = 0;
   let partialSum = 0;
 
   for (let item of arr) {
-      partialSum += item;
-      maxSum = Math.max(maxSum, partialSum);
-      if (partialSum < 0) partialSum = 0;
+    partialSum += item;
+    maxSum = Math.max(maxSum, partialSum);
+    if (partialSum < 0) partialSum = 0;
   }
 
   return maxSum;
@@ -59,15 +63,15 @@ export function getMaxSubSum(arr:number[]) {
 
 
 
-export function camelize(str:string) {
-  
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+export function camelize(str: string) {
+
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 camelize;
 
-export function extractCurrencyValue(str:string){
+export function extractCurrencyValue(str: string) {
 
   return Number(str.replace(/\D/g, ''));
 
 }
- 
+
